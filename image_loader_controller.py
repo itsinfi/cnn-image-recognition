231fbba51f64c8e2369286ea1e32db4528ad7cb7
Zias-Git <j.adam@htp.net>
2025-02-01 19:56:52 +0100

    progress commit doesn't work currently

--- image_loader_controller.py
+++ image_loader_controller.py
@@ -1,7 +1,17 @@
 from src.image_loader.classes.image_loader import ImageLoader
+import os
 
 il = ImageLoader()
 
-img = il.load_image('test.png')
+# load files into a list
+img_file_list = []
+for img_file in os.listdir('data/images'):
+    if img_file.endswith('.png'):
+        img_file_list.append(img_file)
+# read images into a list
+img_list = []
+for img_file in img_file_list:
+    img_list.append(il.load_image(img_file))
 
-il.export_to_csv(img, 'test.csv')
+# export images from a list to a csv
+il.export_list_to_csv(img_list, 'exported.csv')
--- src/image_loader/classes/image_loader.py
+++ src/image_loader/classes/image_loader.py
@@ -9,11 +9,22 @@ class ImageLoader:
     def load_image(self, image_name: str):
 
         img = cv.imread('data/images/'+image_name, cv.IMREAD_GRAYSCALE)
-
+        img = img.flatten()
+        label = [0]
+        # fixme: remove debug
+        print('label: '+str(label))
+        print('img: '+ img)
+        img = label + img
+        print('label + img: '+ img)
         return img
 
+    def export_list_to_csv(self, img_list: list[numpy.ndarray], csv_file_name):
+        with open('data/' + csv_file_name, 'w') as f:
+            csv_writer = csv.writer(f)
+            for img in img_list:
+                csv_writer.writerow(img)
+
     def export_to_csv(self, img: numpy.ndarray, csv_file_name: str):
-        img = img.flatten()
         with open('data/'+csv_file_name, 'w') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerow(img)
