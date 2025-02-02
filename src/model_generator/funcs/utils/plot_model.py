f748913fa8c5b78530b914edf999cbb35317f225
itsinfi <sascha.angermann2002@gmail.com>
2025-02-02 21:28:16 +0100

    comment this out because its annoying lol

--- src/model_generator/funcs/utils/plot_model.py
+++ src/model_generator/funcs/utils/plot_model.py
@@ -8,8 +8,8 @@ def plot_model(model: Sequential, model_name: str):
 
     plot(model, to_file=file_name, show_shapes=True, show_layer_names=True)
 
-    img = plt.imread(file_name)
-    plt.figure(figsize=(10, 10))
-    plt.imshow(img)
-    plt.axis('off')
-    plt.show()
+    # img = plt.imread(file_name)
+    # plt.figure(figsize=(10, 10))
+    # plt.imshow(img)
+    # plt.axis('off')
+    # plt.show()
